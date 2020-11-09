e4ba252a5951aa566f07ee9b3abff6117aba96c4
dedane <evanskeema@gmail.com>
2020-11-09 23:10:10 +0300

    'added-auth'

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import AddFishForm from './AddFishForm';
 import EditFishForm from './EditFishForm';
+import Login from './Login';
+import firebase from 'firebase';
+import base,{ firebaseApp } from '../base';
 
 class Inventory extends React.Component{
+    authHandler = async authData => {
+        //1. Look up the current stire in database
+        const store = await base.fetch(this.props.storeId, { context: this })
+        //2. Claim the store
+        if(!store.owner){
+            //save it as ours
+            await base.post(`${this.props.storeId}/owner`, {
+                data: authData.user.uid
+            })
+        }
+        //3.Set the store as ours    
+            this.setState({
+                uid: authData.user.uid,
+                owner: store.owner || authData.user.uid
+            })
+    }
+    authenticate = (provider) => {
+        const authProvider =new firebase.auth[ `${provider}AuthProvider`]();
+        firebaseApp.auth().signInWithPopup(authProvider).then(this.authHandler);
+    }
     render() {
+        return <Login authenticate={ this.authenticate} />;
         return (
             <div className="Inventory">
             <h2>Inventory</h2>
